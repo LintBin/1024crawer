eb70a4b217a9e204144d9e88d7f4e80507858be2
LintBin <312763889@qq.com>
2014-07-25 18:00:37 +0800

    version 1.1

--- 1024crawler.py
+++ 1024crawler.py
@@ -2,32 +2,23 @@ import urllib.request
 import os
 import re
 from bs4 import BeautifulSoup
-def crawerEach(url,urldir,i):
-    print(len(urldir))
+def crawerEach(url,urldir):
     resp=urllib.request.urlopen(url)
     html=resp.read().decode('gbk')
     soup = BeautifulSoup(html)
     items=soup.find('body').find('div',id='main').find(name='div',attrs={"class":"t","style":"margin:3px auto"}).find('table',id='ajaxtable').find("tbody").findAll(name="tr",attrs={"class":"tr3 t_one"})
-    #f=open("title"+str(i)+".xml",'w',encoding="utf-8")
-    print("this is befor item loop " + str(len(urldir)))
     for item in items:
         target=item.find(name='td',attrs={"style":"text-align:left;padding-left:8px"}).find('h3').find('a')
         if target.u ==  None and target.b == None and target.font == None:
-            urldir[target.text]=target.get('href')
-            print(len(urldir))
-            item_str=str(target)
-            #f.write(item_str+'\n')
-    #f.close()
-    print("after crawerEach " + str(len(urldir)))
-    print("=================================================")
+            urldir[target.text] = "http://wo.yao.cl/"+target.get('href')
     return urldir
     
 def crawer():
     urldir={}
     for i in range(39):
         url="http://wo.yao.cl/thread0806.php?fid=20&page="+str(i+1)
-        print("this is befor crawerEach method " + str(len(urldir)))
-        urldir=crawerEach(url,urldir,i)
+        print("=====================正在爬取第"+str(i+1)+"页=========")
+        urldir=crawerEach(url,urldir)
     f=open("all.xml",'w',encoding="utf-8")
     for key,url in urldir.items() :
         firstColumn="<article title="+"\""+key+"\">"
@@ -36,5 +27,20 @@ def crawer():
         f.write(firstColumn+'\n'+secondColumn+'\n'+thirdColumn+'\n')
     f.close()
 
+def search():
+    keyword = input("请输入关键字：")
+    file=open("all.xml",'r',encoding='utf-8')
+    content=file.read()
+    soup=BeautifulSoup(content)
+    items=soup.findAll(name="article",attrs={"title":re.compile(keyword)})
+    for item in items:
+        print(item.get('title') + item.text)
 if __name__=="__main__":
-    crawer()
+    print("1--更新")
+    print("2--查询")
+    choose=input("请输入结果：")
+    if choose=="1":
+        crawer()
+    else:
+        search()
+    print("The End")
